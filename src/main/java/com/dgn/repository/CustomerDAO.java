3c606faa9076e567ed84009dead660aa9ecff746
osmandgn <oskan001@gmail.com>
2023-01-15 17:10:26 +0300

    V1

---
+++ src/main/java/com/dgn/repository/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.dgn.repository;
+
+import com.dgn.domain.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    public List<Customer> getCustomers();
+
+}
---
+++ src/main/java/com/dgn/repository/CustomerDAOImpl.java
@@ -0,0 +1,25 @@
+package com.dgn.repository;
+
+import com.dgn.domain.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        Session session = sessionFactory.openSession();
+        return session.createQuery("From Customer", Customer.class).getResultList();
+    }
+}
